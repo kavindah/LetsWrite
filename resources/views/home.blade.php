205ca6bb1ff24bde983d6ac07a85c97b8af6a97d
KavindaHarshana <kavinda@silverstripers.com>
2019-08-27 12:46:39 +0530

    scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,11 @@
 @extends('theme.theme')
 
-@section('pageTitle')
-    Home Page
-@endsection
+    @section('pageTitle')
+        Home Page
+    @endsection
 
-@section('content')
-    <h1>this is the Home Page</h1>
-@endsection
+        @include('includes.hero')
+
+    @section('content')
+        <h1>this is the Home Page</h1>
+    @endsection
