96ebe1b6aae0878f0beffce734909f4ef1821962
KavindaHarshana <kavinda@silverstripers.com>
2019-08-29 17:07:23 +0530

    hero link

--- resources/views/includes/hero.blade.php
+++ resources/views/includes/hero.blade.php
@@ -5,7 +5,15 @@
             <div class="col-md-1">
                 <div class="hero-write"><img src="siteImages/writing.png"></div>
             </div>
-            <div class="col-md-3"><h1 class="display-4">Let's Write...<span class="cursor-blink">|</span></h1></div>
+
+            <div class="col-md-4">
+                <a href="/" style="text-decoration: none">
+                    <h1 class="display-4">Let's Write...
+                        <span class="cursor-blink">|</span>
+                    </h1>
+                </a>
+            </div>
+
         </div>
         <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
             featured content or information.</p>
